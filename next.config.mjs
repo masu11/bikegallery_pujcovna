344f04040fa065c1b3a348f9826b0940bbb171ac
masu11 <marcel.suchomel@gmail.com>
2026-09-21 15:36:37 +0200

    Fix GitHub Pages: add basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/bikegallery_pujcovna',
+  assetPrefix: '/bikegallery_pujcovna/',
   images: {
     unoptimized: true,
   },
